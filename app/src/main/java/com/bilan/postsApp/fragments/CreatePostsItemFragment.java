2f91f77f7b19e8c42f9468ba07ffd745ddd416f7
VladyslavBilan <belan2000vlad@gmail.com>
2023-06-06 21:14:27 +0300

    First iteration 6.0

--- app/src/main/java/com/bilan/postsApp/fragments/CreatePostsItemFragment.java
+++ app/src/main/java/com/bilan/postsApp/fragments/CreatePostsItemFragment.java
@@ -1,28 +1,23 @@
 package com.bilan.postsApp.fragments;
 
+import static com.bilan.postsApp.R.id.buttonSubmit;
+import static com.bilan.postsApp.R.id.editTextDescription;
+import static com.bilan.postsApp.R.id.editTextTitle;
+import static com.bilan.postsApp.R.layout.fragment_posts_create;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
 import com.bilan.postsApp.db.DatabaseHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.R.layout.simple_spinner_dropdown_item;
-import static com.bilan.postsApp.R.id.*;
-import static com.bilan.postsApp.R.layout.fragment_posts_create;
-import static com.bilan.postsApp.R.layout.spinner_item_layout;
 
 
 public class CreatePostsItemFragment extends Fragment {
@@ -47,28 +42,19 @@ public class CreatePostsItemFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this.getContext(),
-                spinner_item_layout
-        );
-        adapter.setDropDownViewResource(simple_spinner_dropdown_item);
-
         Button submitButton = view.findViewById(buttonSubmit);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText title = view.findViewById(editTextTitle);
-                EditText description = view.findViewById(editTextDescription);
-                databaseHelper.addNote(
-                        title.getText().toString(),
-                        description.getText().toString()
-                );
-                title.clearFocus();
-                title.clearComposingText();
-                title.clearComposingText();
-                description.clearComposingText();
-                requireActivity().onBackPressed();
-            }
+        submitButton.setOnClickListener(v -> {
+            EditText title = view.findViewById(editTextTitle);
+            EditText description = view.findViewById(editTextDescription);
+            databaseHelper.addNote(
+                    title.getText().toString(),
+                    description.getText().toString()
+            );
+            title.clearFocus();
+            title.clearComposingText();
+            title.clearComposingText();
+            description.clearComposingText();
+            requireActivity().onBackPressed();
         });
     }
 }
